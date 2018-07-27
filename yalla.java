5875277844082a3a5edf3ccc3873fc2b16b12f53
sunnyhitha <41725679+sunnyhitha@users.noreply.github.com>
2018-07-27 14:14:11 +0530

    Create yalla.java

---
+++ yalla.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+      Scanner sc=new Scanner(System.in);
+      int N=sc.nextInt();
+      int K=sc.nextInt();
+      int sum=0;
+      int a[]=new int[N];
+	  for(int i=0;i<a.length;i++)
+       {
+        a[i]=sc.nextInt();
+       }
+      for(int i=0;i<K;i++)
+      {
+       sum=sum+a[i];
+      }
+      System.out.println(sum);  
+	}
+}
